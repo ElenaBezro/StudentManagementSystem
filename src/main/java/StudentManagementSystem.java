89fea104eb7f4a0f36ae0237eae042916ad80f98
Elena Bezrodnova <lenyka_86@mail.ru>
2023-11-03 11:41:58 +0100

    fix bag with countWrongInputAttempts

--- src/main/java/StudentManagementSystem.java
+++ src/main/java/StudentManagementSystem.java
@@ -1,20 +1,26 @@
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class StudentManagementSystem {
-    //List<User> userList = new ArrayList<>();
-    List<String[]> usersPasswords = new ArrayList<>();
+    //TODO: change later to List<User> userList = new ArrayList<>();
+    List<String[]> userToPassword = new ArrayList<>();
     Scanner sc = new Scanner(System.in);
-    private static boolean isOpen = true;
+    private static boolean isRegistration = true;
+    private static boolean isExit = false;
     private static boolean isLoggedIn = false;
+    private static boolean isUserMenuOpen = true;
+    private static int countWrongAttempt = 0;
+    public static final int ALLOWED_WRONG_LOGIN_INPUT = 3;
+
+    //TODO: use enum for roles
+    public String currentUserRole;
 
 
-    public void startSystem(){
+    public void startSystem() {
         System.out.println("Welcome to the Student Management System!");
 
-        while (isOpen && !isLoggedIn) {
+        while (isRegistration && !isLoggedIn) {
             System.out.println("Enter 'login' or 'register' to enter system");
             System.out.println("Enter 'exit' to exit system");
             String command = sc.nextLine();
@@ -28,19 +34,66 @@ public class StudentManagementSystem {
     }
 
     public void login() {
-        isLoggedIn = true;
-        //check if user in system --> check role (use some preinstalled password?) --> enter system with the role
+        while (!isLoggedIn && !isExit) {
+            //TODO: add functionality to exit or exit if input was invalid 3 times
+            System.out.println("Enter login");
+            String login = sc.nextLine();
+            System.out.println("Enter password");
+            String password = sc.nextLine();
+            login(login, password);
+        }
+    }
 
+    private void login(String login, String password) {
+        boolean isUserFound = false;
+
+        for (String[] user : userToPassword) {
+            if (user[0].equals(login) && user[1].equals(password)) {
+                System.out.println("Logged in!");
+                isLoggedIn = true;
+                isUserFound = true;
+                //TODO: pass user's role later (student or teacher)
+                displayCommandsForUser();
+            }
+        }
+        if (!isUserFound) {
+            countWrongAttempt += 1;
+            System.out.println("Invalid login or password!");
+            System.out.println("Attempts availible: " + (ALLOWED_WRONG_LOGIN_INPUT - countWrongAttempt));
+            if (countWrongAttempt >= ALLOWED_WRONG_LOGIN_INPUT) {
+                exit();
+            }
+        }
+        //TODO: check if user in system --> check role (use some preinstalled password?) --> enter system with the role
     }
 
     public void registerUser() {
-        //add a new user to the system. Check permission and assign a role.
-        //login(newUser data);
+        System.out.println("Enter new login");
+        String login = sc.nextLine();
+        System.out.println("Enter password");
+        String password = sc.nextLine();
+
+        // TODO: Check permission (store predefined keywords for roles?) and assign a role.
+        userToPassword.add(new String[]{login, password});
+
+        login(login, password);
     }
 
     public void exit() {
-        isOpen = false;
+        isRegistration = false;
+        isExit = true;
         System.out.println("Goodbye!");
         sc.close();
+    }
+
+    public void displayCommandsForUser() {
+        while (isUserMenuOpen) {
+            System.out.println("Enter command");
+            String command = sc.nextLine();
+            switch (command.toLowerCase()) {
+                case "exit" -> exit();
+                default -> System.out.println("Invalid input");
+            }
+        }
     }
 }
